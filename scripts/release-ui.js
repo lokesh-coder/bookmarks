c5de527c0ade61a301e953be413f4d0453681817
inbot <mexican.dirtyfellow@gmail.com>
2020-01-01 17:16:10 +0530

    c55

--- scripts/release-ui.js
+++ scripts/release-ui.js
@@ -3,6 +3,8 @@ function ReleaseUI(message = "auto commit") {
   ghpages.publish("public", { message }, function(err) {
     if (!err) {
       console.log("successfully publihsed ui");
+    } else {
+      console.log("err", err);
     }
   });
 }
