e98d0c4073f906027bbcd0ef9d99c6234c09fa4e
Lokesh r <lokeshr@IN-Lokesh.local>
2020-01-01 12:09:00 +0530

    c18

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -6,7 +6,7 @@ try {
   const mndata = core.getInput("mndata");
   const data = JSON.parse(mndata);
   data.updatedAt = new Date().toTimeString();
-  // const payload = JSON.stringify(github.context.payload, undefined, 2);
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   app.AddBookmark(data);
 } catch (error) {
   core.setFailed(error.message);
