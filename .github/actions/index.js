146d00c6eb7c54e4db3d4aab434a5ae6bb5aa10d
Lokesh r <lokeshr@IN-Lokesh.local>
2020-01-01 10:29:27 +0530

    c8

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -2,6 +2,7 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
+  console.log(`===>: ${github.event.client_payload}`);
   const mndata = core.getInput("mndata");
   console.log("mndata", mndata);
   const payload = JSON.stringify(github.context.payload, undefined, 2);
