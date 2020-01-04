665c86bf273a94eb0c3cd7a096f7a2b974891fc4
inbot <mexican.dirtyfellow@gmail.com>
2020-01-04 10:48:17 +0530

    c75

--- scripts/build-repo.js
+++ scripts/build-repo.js
@@ -10,11 +10,37 @@ function BuildRepo(bookmark) {
   }
   dataFileContent = JSON.parse(dataFileContent);
 
-  const isBookmarkExists = dataFileContent.bookmarks.find(
-    b => b.url === bookmark.url
-  );
-  if (!isBookmarkExists) {
-    dataFileContent.bookmarks.push(bookmark);
+  if (bookmark.type == "add") {
+    const isBookmarkExists = dataFileContent.bookmarks.find(
+      b => b.url === bookmark.url
+    );
+    if (!isBookmarkExists) {
+      dataFileContent.bookmarks.push(bookmark);
+    } else {
+      dataFileContent.bookmarks = dataFileContent.bookmarks(b =>
+        b.id == bookmark.id ? bookmark : b
+      );
+    }
+    jetpack.write(dataFilePath, JSON.stringify(dataFileContent, null, 2));
+  }
+  if (bookmark.type == "remove") {
+    dataFileContent.bookmarks = dataFileContent.bookmarks.filter(
+      b => b.id == bookmark.id
+    );
+    jetpack.write(dataFilePath, JSON.stringify(dataFileContent, null, 2));
+  }
+  if (bookmark.type == "fav") {
+    dataFileContent.bookmarks = dataFileContent.bookmarks.map(b => {
+      if (b.id == bookmark.id) {
+        b.isFav = !bookmark.isFav;
+        return b;
+      }
+    });
+    jetpack.write(dataFilePath, JSON.stringify(dataFileContent, null, 2));
+  }
+
+  if (bookmark.type == "removeall") {
+    dataFileContent.bookmarks = [];
     jetpack.write(dataFilePath, JSON.stringify(dataFileContent, null, 2));
   }
 }
